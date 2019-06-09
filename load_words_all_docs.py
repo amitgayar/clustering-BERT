41eb682bc85fb5730bdf484a43156667741f170e
amitgayar <gayaramit@gmail.com>
2019-06-09 11:32:16 +0530

    second commit

--- load_words_all_docs.py
+++ load_words_all_docs.py
@@ -4,22 +4,22 @@ from nltk.stem import PorterStemmer
 
 from os import listdir
 from os.path import isfile, join
-mypath = '../news-analyzer/data/rafale deal/news/'
+mypath = 'news-analyzer/data/rafale deal/news/'
 news_text_file = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
 
 # ---remove punctuations; code from : https://towardsdatascience.com/working-with-text-data-from-quality-to-quantity-1e9d8aa773dd
 
-# ps = PorterStemmer()
-# def str_clean(text):
-#     punct = '():[]?.,|_^-&><;!"/%'  
-#     table = str.maketrans(punct, ' '*len(punct), "0123456789$#'=")
-#     cleaned_comment = []
-#     # re.findall(r"[\w']+", text)
-#     for word in text.split():
-#         cleaned_comment.extend(word.translate(table).split())
-#         cleaned_comment = [x.lower() for x in cleaned_comment]
-#         cleaned_comment = [ps.stem(word) for word in cleaned_comment]
-#     return cleaned_comment                                                                                                                                                                       
+ps = PorterStemmer()
+def str_clean(text):
+    punct = '():[]?.,|_^-&><;!"/%'  
+    table = str.maketrans(punct, ' '*len(punct), "0123456789$#'=")
+    cleaned_comment = []
+    # re.findall(r"[\w']+", text)
+    for word in text.split():
+        cleaned_comment.extend(word.translate(table).split())
+        cleaned_comment = [x.lower() for x in cleaned_comment]
+        cleaned_comment = [ps.stem(word) for word in cleaned_comment]
+    return cleaned_comment                                                                                                                                                                       
 
 # ------spacy cleaning
 
@@ -56,10 +56,11 @@ bow = []
 for f in news_text_file:
     
     text_article = pickle.load(open(f, 'rb')) 
+    text_article['content'] = " ".join(str_clean(text_article['content']))
     text_article['Text_After_Clean'] = normalize(text_article['content'], lowercase=True, remove_stopwords=True)
     bow.append(text_article['Text_After_Clean'])
     # print (type(text_article['Text_After_Clean']))
-    print ('article no : ',news_text_file.index(f), text_article['Text_After_Clean'],'\n')
+    print ('article no : ',news_text_file.index(f), sorted(text_article['Text_After_Clean']),'\n')
     # bow.append(str_clean(text_article['content'])) 
 print ('\n','type of variable : ',type(bow))
 store_file = {}
@@ -68,4 +69,4 @@ print (wordSet,'\n',len(wordSet))
 store_file['wordset'] = wordSet
 store_file['clean_text_doclist'] = bow 
 
-pickle.dump(store_file,open('load_words_all_docs_spacy_second_pruning.pkl','wb'))
+pickle.dump(store_file,open('load_words_all_docs_spacy.pkl','wb'))
--- load_words_all_docs_old.py
+++ load_words_all_docs_old.py
@@ -4,22 +4,22 @@ from nltk.stem import PorterStemmer
 
 from os import listdir
 from os.path import isfile, join
-mypath = '../news-analyzer/data/rafale deal/news/'
+mypath = 'news-analyzer/data/rafale deal/news/'
 news_text_file = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
 
 # ---remove punctuations; code from : https://towardsdatascience.com/working-with-text-data-from-quality-to-quantity-1e9d8aa773dd
 
-ps = PorterStemmer()
-def str_clean(text):
-    punct = '():[]?.,|_^-&><;!"/%'  
-    table = str.maketrans(punct, ' '*len(punct), "0123456789$#'=")
-    cleaned_comment = []
-    # re.findall(r"[\w']+", text)
-    for word in text.split():
-        cleaned_comment.extend(word.translate(table).split())
-        cleaned_comment = [x.lower() for x in cleaned_comment]
-        cleaned_comment = [ps.stem(word) for word in cleaned_comment]
-    return cleaned_comment                                                                                                                                                                       
+# ps = PorterStemmer()
+# def str_clean(text):
+#     punct = '():[]?.,|_^-&><;!"/%'  
+#     table = str.maketrans(punct, ' '*len(punct), "0123456789$#'=")
+#     cleaned_comment = []
+#     # re.findall(r"[\w']+", text)
+#     for word in text.split():
+#         cleaned_comment.extend(word.translate(table).split())
+#         cleaned_comment = [x.lower() for x in cleaned_comment]
+#         cleaned_comment = [ps.stem(word) for word in cleaned_comment]
+#     return cleaned_comment                                                                                                                                                                       
 
 # ------spacy cleaning
 
@@ -56,11 +56,10 @@ bow = []
 for f in news_text_file:
     
     text_article = pickle.load(open(f, 'rb')) 
-    text_article['content'] = " ".join(str_clean(text_article['content']))
     text_article['Text_After_Clean'] = normalize(text_article['content'], lowercase=True, remove_stopwords=True)
     bow.append(text_article['Text_After_Clean'])
     # print (type(text_article['Text_After_Clean']))
-    print ('article no : ',news_text_file.index(f), sorted(text_article['Text_After_Clean']),'\n')
+    print ('article no : ',news_text_file.index(f), text_article['Text_After_Clean'],'\n')
     # bow.append(str_clean(text_article['content'])) 
 print ('\n','type of variable : ',type(bow))
 store_file = {}
